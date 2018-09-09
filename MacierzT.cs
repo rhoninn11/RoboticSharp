ed6584e831021de08c30b46f2540074613d88a23
olindenaus <madorgon@gmail.com>
2018-09-09 15:04:19 +0200

    iloczyn wektorowy, macierz*wektor, T04 i poprawka w transpozycji

--- MacierzT.cs
+++ MacierzT.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 public class MacierzT
@@ -7,6 +9,26 @@ public class MacierzT
     public MacierzT()
     {
         data = new double[4, 4];
+    }
+    public MacierzT(double theta_i, double alpha_i_minus_1) //na przysz³oœæ ¿eby mo¿na by³o te¿ tworzyæ macierz rotacji
+    {
+        data = new double[3,3];
+
+        double theta_radians = ConvertToRadians(theta_i);
+        double alpha_radians = ConvertToRadians(alpha_i_minus_1);
+
+        data[0, 0] = Math.Cos(theta_radians);
+        data[0, 1] = -Math.Sin(theta_radians);
+        data[0, 2] = 0;
+        
+        data[1, 0] = Math.Sin(theta_radians) * Math.Cos(alpha_radians);
+        data[1, 1] = Math.Cos(theta_radians) * Math.Cos(alpha_radians);
+        data[1, 2] = Math.Sin(alpha_radians);
+        
+        data[2, 0] = Math.Sin(theta_radians) * Math.Sin(alpha_radians);
+        data[2, 1] = Math.Cos(theta_radians) * Math.Sin(alpha_radians);
+        data[2, 2] = Math.Cos(alpha_radians);
+
     }
     public MacierzT(double theta_i, double alpha_i_minus_1, double a_i_minus_1, double d_i)
     {
@@ -41,24 +63,53 @@ public class MacierzT
 
     public MacierzT TranspozycjaMacierzT()
     {
-        MacierzT macierzOdwrotna = new MacierzT();
-
-        for (int wiersz = 0; wiersz < 4; wiersz++)
+        MacierzT macierzTransponowana = new MacierzT();
+        int size = this.data.GetLength(0);
+        for (int wiersz = 0; wiersz < size; wiersz++)
         {
-            for (int kolumna = 0; kolumna < 4; kolumna++)
+            for (int kolumna = 0;  kolumna < size; kolumna++)
             {
-                macierzOdwrotna.data[kolumna, wiersz] = this.data[wiersz, kolumna];
+                macierzTransponowana.data[kolumna, wiersz] = this.data[wiersz, kolumna];
             }
         }
-        return macierzOdwrotna;
+        return macierzTransponowana;
     }
 
+    public MacierzT MacierzT0_4(MacierzT[] od0do4)
+    {
+        return od0do4[0] * od0do4[1] * od0do4[2] * od0do4[3];
+    }
+
+    /// <summary>
+    /// Macierz * wektor
+    /// </summary>
+    public static List<double> operator *(MacierzT m, List<double> v)
+    {
+        List<double> vector = new List<double>();
+        int size = m.data.GetLength(0);
+        double element;
+        for (int i = 0; i < size; i++)
+        {
+            element = 0;
+            for (int j = 0; j < size; j++)
+            {
+                element += m.data[size, j] * v[j];
+            }
+            vector.Add(element);
+        }
+        return vector;
+    }
+
+    /// <summary>
+    /// Macierz * macierz
+    /// </summary>
     public static MacierzT operator *(MacierzT m1, MacierzT m2)
     {
         MacierzT resultMatrix = new MacierzT();
-        for (int wiersz = 0; wiersz < 4; wiersz++)
+        int size = m1.data.GetLength(0);
+        for (int wiersz = 0; wiersz < size; wiersz++)
         {
-            for (int kolumna = 0; kolumna < 4; kolumna++)
+            for (int kolumna = 0; kolumna < size; kolumna++)
             {
                 for (int i=0; i < 4; i++)
                 {
@@ -67,7 +118,6 @@ public class MacierzT
             }
         }
         return resultMatrix;
-
     }
 
     public double ConvertToRadians(double angle)
