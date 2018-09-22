ca27eca30ec1bfae04120aecc084588b8d3d892c
Marcin <rhoninn11@gmail.com>
2018-09-22 15:49:12 +0200

    conflict resolve fix

--- RoboticSharp.App/Program.cs
+++ RoboticSharp.App/Program.cs
@@ -14,12 +14,8 @@ namespace RoboticSharp.App
             //test mnożenia
             Matrix t1 = new Matrix(new Symbol("q1"), new Symbol(90), new Symbol(0), new Symbol(0));
             Matrix t2 = new Matrix(new Symbol("q2"), new Symbol(90), new Symbol(0), new Symbol(0));
-<<<<<<< HEAD
-            Matrix t3 = t1.TranspozycjaMacierz();
-=======
             Matrix t3 = t1.MatrixTransposition();
             
->>>>>>> b81ccdc9f3a54904b4d826711236e44cc07d3c8b
 
             Console.WriteLine("===================");
             Console.WriteLine(t1.ToString());
